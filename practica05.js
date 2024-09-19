6f6effb2a73a8a38ab8c0d2dc580fe8f3c4fb76d
miguelg083 <miguelgarcia08032@gmail.com>
2024-09-19 14:44:28 -0600

    cambios en estos archivos

--- practica05.js
+++ practica05.js
@@ -1,30 +1,41 @@
 const app = document.getElementById("app")
 const url = "https://picsum.photos/id/"
 const size = "/200/200"
-console.log(app)
-console.log(url)
+
 const picture = parametro => {
     const src = url + parametro + size
     return `
-            <figure>
-                <img src="https://picsum.photos/id/${parametro}/200/200" alt="" id="${parametro}">
-                <figcaption>${parametro}</figcaption>
-            </figure>
-            `
+    <figure>
+        <img src="https://picsum.photos/id/${parametro}/200/200" alt="" id="${parametro}" class="">
+        <figcaption>${parametro}</figcaption>
+    </figure>
+        `
 }
 
 //console.log(picture())
 
 app.innerHTML = picture(1) + picture(2) + picture(3)
 
-const elemento1 = document.getElementById("1")
-const elemento2 = document.getElementById("2")
-const elemento3 = document.getElementById("3")
-elemento.addEventListener("click",miFuncion)
-elemento.addEventListener("click",miFuncion)
-elemento.addEventListener("click",miFuncion)
+//const elemento1 = document.getElementById("1")
+//const elemento2 = document.getElementById("2")
+//const elemento3 = document.getElementById("3")
+//elemento1.addEventListener("click", miFuncion)
+//elemento2.addEventListener("click", miFuncion)
+//elemento3.addEventListener("click", miFuncion)
 
-function miFuncion(parametro){
+const lista = document.querySelectorAll("img")
+
+//simplifica lo de arriba
+for(let i=0; i<lista.length; i++){
+    //con esto solo pasar el mouse cambia la forma
+    //simplifica de hacer lo de abajo
+    lista[i].addEventListener("mouseenter", (evento) => {evento.target.classList.toggle("redonda")})
+}
+
+//con esto se hace el movimiento de click y cambiar
+function miFuncion(evento) {
     console.log("hayyy: ")
-    console.log(parametro)
+    console.log(parametro.target)
+    var imagen = parametro.target
+    imagen.classList.toggle("redonda")
 }
